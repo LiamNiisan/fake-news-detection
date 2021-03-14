a778da6e5cad510350c56c09fa3099546f2acd20
Badr Jaidi <badrj48@gmail.com>
2021-03-14 14:45:36 -0700

    numpy for Preprocessing

--- src/Preprocessing.py
+++ src/Preprocessing.py
@@ -26,47 +26,50 @@ class Preprocessing:
     def remove_punct(self, data):
 
         corpus_df = data['title'] + data['text']
+        corpus = corpus_df.to_numpy()
 
-        for i in range(corpus_df.shape[0]):
-            text = str(corpus_df.iloc[i])
+        for i in range(len(corpus)):
+            text = corpus[i]
             text = text.lower()  # Supprimer les majuscules
             text = text.replace("'", " ")  # Remplacer apostrophe par une espace
             text = text.replace("\n", " ") # Supprimer les retours chariot
             text = text.translate(str.maketrans("", "", string.punctuation)) # Supprimer la ponctuation
             text = text.encode('utf-8').decode('utf-8') # Éliminations des caractères spéciaux et accentués
-            corpus_df.iloc[i] = text
+            corpus[i] = text
 
-        return corpus_df
+        return corpus
         
 
-    def tokenize(self, corpus_df):
+    def tokenize(self, corpus):
 
-        return [nltk.word_tokenize(corpus_df.iloc[i]) for i in range(corpus_df.shape[0])]
+        return np.array([nltk.word_tokenize(corpus[i]) for i in range(len(corpus))])
 
 
     def remove_stop_words(self, corpus):
 
         stop_words = set(stopwords.words('english'))
-        filtered_corpus = []
-        for text in corpus:
-            filtered_corpus.append([word for word in text if not word in stop_words])
+        filtered_corpus = np.array([None] * len(corpus))
+        for i in range(len(corpus)):
+            filtered_corpus[i] = np.array([word for word in corpus[i] if not word in stop_words])
             
         return filtered_corpus
 
     
     def lemmatize(self, corpus):
 
-        lemma_corpus = []
+        lemma_corpus = np.array([None] * len(corpus))
         wordnet_lemmatizer = WordNetLemmatizer()
 
-        for text in corpus:
-            lemma_text = []    
-            for word in text:
+        for i in range(len(corpus)):
+            text = corpus[i]
+            lemma_text = np.array([None] * len(text)) 
+            for j in range(len(text)):
+                word = text[j]
                 word = wordnet_lemmatizer.lemmatize(word, pos = "n")
                 word = wordnet_lemmatizer.lemmatize(word, pos = "v")
                 word = wordnet_lemmatizer.lemmatize(word, pos = ("a"))
-                lemma_text.append(word)       
-            lemma_corpus.append(lemma_text)
+                lemma_text[j] = word  
+            lemma_corpus[i] = lemma_text
 
         return lemma_corpus
 
@@ -74,10 +77,10 @@ class Preprocessing:
     def run(self, path):
 
         data = self.get_data(path)
-        corpus_df = self.remove_punct(data)
-        corpus = np.array(self.tokenize(corpus_df))
-        corpus = np.array(self.remove_stop_words(corpus))
-        corpus = np.array(self.lemmatize(corpus))
+        corpus = self.remove_punct(data)
+        corpus = self.tokenize(corpus)
+        corpus = self.remove_stop_words(corpus)
+        corpus = self.lemmatize(corpus)
 
         return corpus
 
