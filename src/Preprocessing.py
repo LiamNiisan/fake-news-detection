68fd01c66a2bd05e70ebd21fb19c310517bc1fb8
Badr Jaidi <badrj48@gmail.com>
2021-03-14 20:06:27 -0700

    Combined Preprocessing and decomposition to be able to be used together

--- src/Preprocessing.py
+++ src/Preprocessing.py
@@ -14,47 +14,47 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
+
 class Preprocessing:
 
     def get_data(self, path):
 
         data = pd.read_excel(path)
-
-        return data
-
-
-    def remove_punct(self, data):
-
         corpus_df = data['title'] + data['text']
         corpus = corpus_df.to_numpy()
+
+        return corpus
+
+    def remove_punct(self, corpus):
 
         for i in range(len(corpus)):
             text = corpus[i]
             text = text.lower()  # Supprimer les majuscules
-            text = text.replace("'", " ")  # Remplacer apostrophe par une espace
-            text = text.replace("\n", " ") # Supprimer les retours chariot
-            text = text.translate(str.maketrans("", "", string.punctuation)) # Supprimer la ponctuation
-            text = text.encode('utf-8').decode('utf-8') # Éliminations des caractères spéciaux et accentués
+            # Remplacer apostrophe par une espace
+            text = text.replace("'", " ")
+            text = text.replace("\n", " ")  # Supprimer les retours chariot
+            # Supprimer la ponctuation
+            text = text.translate(str.maketrans("", "", string.punctuation))
+            # Éliminations des caractères spéciaux et accentués
+            text = text.encode('utf-8').decode('utf-8')
             corpus[i] = text
 
         return corpus
-        
 
     def tokenize(self, corpus):
 
         return np.array([nltk.word_tokenize(corpus[i]) for i in range(len(corpus))])
-
 
     def remove_stop_words(self, corpus):
 
         stop_words = set(stopwords.words('english'))
         filtered_corpus = np.array([None] * len(corpus))
         for i in range(len(corpus)):
-            filtered_corpus[i] = np.array([word for word in corpus[i] if not word in stop_words])
-            
+            filtered_corpus[i] = np.array(
+                [word for word in corpus[i] if not word in stop_words])
+
         return filtered_corpus
 
-    
     def lemmatize(self, corpus):
 
         lemma_corpus = np.array([None] * len(corpus))
@@ -62,26 +62,22 @@ class Preprocessing:
 
         for i in range(len(corpus)):
             text = corpus[i]
-            lemma_text = np.array([None] * len(text)) 
+            lemma_text = np.array([None] * len(text))
             for j in range(len(text)):
                 word = text[j]
-                word = wordnet_lemmatizer.lemmatize(word, pos = "n")
-                word = wordnet_lemmatizer.lemmatize(word, pos = "v")
-                word = wordnet_lemmatizer.lemmatize(word, pos = ("a"))
-                lemma_text[j] = word  
-            lemma_corpus[i] = lemma_text
+                word = wordnet_lemmatizer.lemmatize(word, pos="n")
+                word = wordnet_lemmatizer.lemmatize(word, pos="v")
+                word = wordnet_lemmatizer.lemmatize(word, pos=("a"))
+                lemma_text[j] = word
+            lemma_corpus[i] = ' '.join(lemma_text)
 
         return lemma_corpus
 
+    def preprocess(self, data):
 
-    def run(self, path):
-
-        data = self.get_data(path)
         corpus = self.remove_punct(data)
         corpus = self.tokenize(corpus)
         corpus = self.remove_stop_words(corpus)
         corpus = self.lemmatize(corpus)
 
         return corpus
-
-
--- src/decomposition.py
+++ src/decomposition.py
@@ -5,55 +5,63 @@
 #   segmenter les fichiers, supprimer les mots vides et effectuer la lemmatisation.
 
 # librairies utilisees
+import os
+import sys
+from tqdm import tqdm
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import KFold
-import csv
-import os, sys
 from sklearn.model_selection import train_test_split
-
-# kfold decomposition
-X = np.array(source)
-kf = KFold(n_splits=5,random_state=2, shuffle=True)
-kf.get_n_splits(X)
-donne_true  = []
-for train_index, test_index in kf.split(source):
-    print("TRAIN:", train_index, "TEST:", test_index)
-    X_train, X_test = X[train_index], X[test_index]
-    donne_true.append([X_train, X_test])
-
-Y = np.array(source)
-kf = KFold(n_splits=5,random_state=2, shuffle=True)
-kf.get_n_splits(Y)
-donne_false  = []
-for train_index, test_index in kf.split(source):
-    print("TRAIN:", train_index, "TEST:", test_index)
-    Y_train, Y_test = Y[train_index], Y[test_index]
-    donne_false.append([Y_train, Y_test])    
-
-# create training folder
-cwd = os.getcwd()
-training = cwd + '\data\\training' 
-print(training)
-os.mkdir(training)
-
-# create kfold folder in training folder
-kfold_n = 5
-for i in range(kfold_n):
-    data_set_path = cwd + '\data\\training\data_set_' + str(i+1)
-    os.mkdir(data_set_path)
-    os.mkdir(data_set_path+'\\test')
-    pd.DataFrame(donne_true[i][0]).to_excel(data_set_path+'\\test\\true.xlsx')  
-    pd.DataFrame(donne_false[i][0]).to_excel(data_set_path+'\\test\\false.xlsx')   
-    os.mkdir(data_set_path+'\\train')
-    os.mkdir(data_set_path+'\\train\\vc')
-    X_train, X_val = train_test_split(donne_true[i][1], test_size=0.20, random_state = 1) 
-    pd.DataFrame(X_train).to_excel(data_set_path+'\\train\\true.xlsx')  
-    pd.DataFrame(X_val).to_excel(data_set_path+'\\train\\vc\\true.xlsx')
-    Y_train, Y_val = train_test_split(donne_false[i][1], test_size=0.20, random_state = 1) 
-    pd.DataFrame(Y_train).to_excel(data_set_path+'\\train\\false.xlsx')  
-    pd.DataFrame(Y_val).to_excel(data_set_path+'\\train\\vc\\false.xlsx')   
-    
+from Preprocessing import Preprocessing
 
 
+def kfold_decompose(data, kfold_n):
+
+    X = np.array(data)
+    kf = KFold(n_splits=kfold_n, random_state=2, shuffle=True)
+    data_output = []
+
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        data_output.append([X_train, X_test])
+
+    return data_output
 
 
+def create_folders(data_label_true, data_label_false, kfold_n, cwd='../data/'):
+
+    training = cwd + 'training/'
+    os.mkdir(training)
+    prp = Preprocessing()
+
+    for i in tqdm(range(kfold_n)):
+
+        data_set_path = cwd + training + 'data_set_' + str(i+1)
+
+        os.mkdir(data_set_path)
+        os.mkdir(data_set_path+'/test')
+        os.mkdir(data_set_path+'/train')
+        os.mkdir(data_set_path+'/train/vc')
+
+        pd.DataFrame(data_label_true[i][0]).to_excel(
+            data_set_path+'/test/true.xlsx')
+        pd.DataFrame(data_label_false[i][0]).to_excel(
+            data_set_path+'/test/false.xlsx')
+
+        X_train, X_val = train_test_split(
+            data_label_true[i][1], test_size=0.20, random_state=1)
+        pd.DataFrame(prp.preprocess(X_train)).to_excel(data_set_path+'/train/true.xlsx')
+        pd.DataFrame(X_val).to_excel(data_set_path+'/train/vc/true.xlsx')
+
+        X_train, X_val = train_test_split(
+            data_label_false[i][1], test_size=0.20, random_state=1)
+        pd.DataFrame(prp.preprocess(X_train)).to_excel(data_set_path+'/train/false.xlsx')
+        pd.DataFrame(X_val).to_excel(data_set_path+'/train/vc/false.xlsx')
+
+
+def main(true_set, false_set, kfold_n=5):
+
+    data_label_true = kfold_decompose(true_set, kfold_n)
+    data_label_false = kfold_decompose(false_set, kfold_n)
+
+    create_folders(data_label_true, data_label_false, kfold_n)
